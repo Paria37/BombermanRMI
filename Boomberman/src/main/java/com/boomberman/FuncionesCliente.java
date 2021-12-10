3b60e2dfeee76b249be1f2a852a0fc0a90a9bcfd
angel_21 <gjanegel17@gmail.com>
2021-12-09 22:10:35 -0600

    proto_cliente

---
+++ Boomberman/src/main/java/com/boomberman/FuncionesCliente.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.boomberman;
+
+/**
+ *
+ * @author angel_23
+ */
+public class FuncionesCliente implements ICliente{
+
+    @Override
+    public InformacionPartida solicitarNuevaPartida(int N) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean solicitarNuevoJugador(String nombre) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean estaListaPartida() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void movimientoCLiente(short id, short x, short y) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void ponerBomba(short x, short y) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public InformacionPartida obtenerEstadoJuego() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean eliminacion() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+}
